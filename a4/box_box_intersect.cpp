c1d8889cbd3e691064fd7679fb9ce558a74dd7e6
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-10 23:43:32 -0500

    Update

--- a4/box_box_intersect.cpp
+++ a4/box_box_intersect.cpp
@@ -5,7 +5,7 @@ bool box_box_intersect(
 {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
-  bool has_intersect;
+  bool has_intersect = true;
   has_intersect = has_intersect && A.min_corner(0) < B.max_corner(0) && A.min_corner(1) < B.max_corner(1) && A.min_corner(2) < B.max_corner(2);
   has_intersect = has_intersect && A.max_corner(0) > B.min_corner(0) && A.max_corner(1) > B.min_corner(1) && A.max_corner(2) > B.min_corner(2);
   return has_intersect;
