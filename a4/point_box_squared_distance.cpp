a34635f767f000c11241bc960ce8b2296fb1f2dc
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-10 22:06:37 -0500

    Update

--- a4/point_box_squared_distance.cpp
+++ a4/point_box_squared_distance.cpp
@@ -1,4 +1,5 @@
 #include "point_box_squared_distance.h"
+#include<cmath>
 
 double point_box_squared_distance(
   const Eigen::RowVector3d & query,
@@ -6,7 +7,10 @@ double point_box_squared_distance(
 {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here
-  // https://en.wikipedia.org/wiki/Euclidean_distance#Squared_Euclidean_distance
-  return 0;
+  double d = 0;
+  d += pow(std::max(std::max(box.min_corner[0] - query[0], query[0] - box.max_corner[0]), 0.0), 2);
+  d += pow(std::max(std::max(box.min_corner[2] - query[2], query[2] - box.max_corner[2]), 0.0), 2);
+  d += pow(std::max(std::max(box.min_corner[2] - query[2], query[2] - box.max_corner[2]), 0.0), 2);
+  return d;
   ////////////////////////////////////////////////////////////////////////////
 }
