228b48b19a7b7528a6c8c7dd39a9cfb07c2cb5f5
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-10 21:23:03 -0500

    Update

--- a4/nearest_neighbor_brute_force.cpp
+++ a4/nearest_neighbor_brute_force.cpp
@@ -1,5 +1,7 @@
 #include "nearest_neighbor_brute_force.h"
 #include <limits>// std::numeric_limits<double>::infinity();
+// #include <math.h>       /* pow */
+#include<cmath>
 
 void nearest_neighbor_brute_force(
   const Eigen::MatrixXd & points,
@@ -11,5 +13,19 @@ void nearest_neighbor_brute_force(
   // Replace with your code here:
   I = -1;
   sqrD = 0;
+  int i;
+  Eigen::RowVector3d d;
+  bool first = true;
+  int num_points = points.rows();
+  double cur_sqrd;
+  for (i=0; i<num_points; i++){
+    d = points.row(i)-query;
+    cur_sqrd = sqrt(pow(d[0], 2)+pow(d[1], 2)+pow(d[2], 2));
+    if (first || cur_sqrd<sqrD){
+      first = false;
+      sqrD = cur_sqrd;
+      I = i;
+    }
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
