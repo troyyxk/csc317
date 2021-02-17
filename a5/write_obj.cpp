f1e21b344433abbf56460242b263e31993b98ff9
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-17 17:27:13 -0500

    Finsih write obj

--- a5/write_obj.cpp
+++ a5/write_obj.cpp
@@ -16,5 +16,47 @@ bool write_obj(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
   ////////////////////////////////////////////////////////////////////////////
-  return false;
+  // http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
+  // https://eigen.tuxfamily.org/dox/group__TutorialMatrixClass.html
+
+  std::ofstream file;
+  file.open(filename);
+  if (!file.is_open()) {
+    return false;
+  }
+
+  int i;
+
+  // v is a vertex
+  // v 1.000000 -1.000000 -1.000000
+  for (i=0; i<V.rows(); i++) {
+    file << "V" << " " << V(i, 0) << " " << V(i, 1) << " " << V(i, 2) << "\n";
+  }
+
+  // vt is the texture coordinate of one vertex
+  // vt 0.748573 0.750412
+  // 2D parametrization positions UV
+  for (i=0; i<UV.rows(); i++) {
+    file << "vt" << " " << UV(i, 0) << " " << UV(i, 1) << "\n";
+  }
+
+  // vn is the normal of one vertex
+  // vn 0.000000 0.000000 -1.000000
+  // 3D normal vectors NV
+  for (i=0; i<NV.rows(); i++) {
+    file << "vn" << " " << NV(i, 0) << " " << NV(i, 1) << " " << NV(i, 2) << "\n";
+  }
+
+  // f is a face
+  // f 5/1/1 1/2/1 4/3/1
+  // vertex ind/texture coordinate/normal ind
+  for (i=0; i<F.rows(); i++) {
+    file << "f";
+    file << " " << F(i, 0)+1 << "/" << UF(i, 0)+1 << "/" << NF(i,0)+1;
+    file << " " << F(i, 1)+1 << "/" << UF(i, 1)+1 << "/" << NF(i,1)+1;
+    file << " " << F(i, 2)+1 << "/" << UF(i, 2)+1 << "/" << NF(i,2)+1;
+    file << "\n";
+  }
+
+  return true;
 }
