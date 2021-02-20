645650e94ab523cb083150182ede51e6f2749396
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-20 18:23:59 -0500

    Update

--- a5/make.sh
+++ a5/make.sh
@@ -4,13 +4,13 @@
 # cmake ..
 # make
 
-cd C:\Users\yxk\Documents\github\2021_winter_term\CSC317\assignments\a5\computer-graphics-meshes
+# cd C:\Users\yxk\Documents\github\2021_winter_term\CSC317\assignments\a5\computer-graphics-meshes
 
 rm build_release -rf
 mkdir build_release
 cd build_release
-cmake -DCMAKE_BUILD_TYPE=Release .. 
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
 # cmake --config Release -DCMAKE_BUILD_TYPE=Release .. 
 # cmake --build {BUILD_DIR} --config Release
 # cmake -DCMAKE_BUILD_TYPE=Release --no-warn-unused-cli ..
-make 
--- a5/sphere.cpp
+++ a5/sphere.cpp
@@ -13,5 +13,56 @@ void sphere(
 {
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
+  // https://en.wikipedia.org/wiki/Spherical_coordinate_system
+  int total_face_num = num_faces_u * num_faces_v;
+  int total_vertex_num = (num_faces_u + 1) * (num_faces_v + 1);
+
+  V.resize(total_vertex_num,3);
+  F.resize(total_face_num,4);
+  UV.resize(total_vertex_num,2);
+  UF.resize(total_face_num,4);
+  NV.resize(total_vertex_num,3);
+  NF.resize(total_face_num,4);
+
+  const double pi = 355/113; 
+  double x, y, z, r, u, v, theta, ft, phi, fp;
+  int i, j, ind;
+  r = 1.0;
+  ft = pi/num_faces_u;
+  fp = (2*pi)/num_faces_v;
+
+  ind = 0;
+  for (i=0; i<(num_faces_u + 1); i++){
+    for (j=0; j<(num_faces_v + 1); j++){
+      theta = ft *i;
+      phi = fp*j;
+
+      x = r*sin(theta)*cos(phi);
+      y = r*sin(theta)*sin(phi);
+      z = r*cos(theta);
+
+      V.row(ind) = Eigen::RowVector3d(x, y, z);
+      UV(ind, 1) = i/num_faces_u;
+      UV(ind, 2) = j/num_faces_v;
+      NV.row(ind) = Eigen::RowVector3d(x, y, z);
+      ind++;
+    }
+  }
+
+  int c1, c2, c3, c4;
+  ind = 0;
+  for (i=0; i<num_faces_u; i++){
+    for (j=0; j<num_faces_v; j++){
+      c1 = i * (num_faces_v+1) + j;
+      c2 = c1 +1;
+      c4 = (i+1) * (num_faces_v+1) + j;
+      c3 = c4 + 1;
+      F.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
+      F.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
+      F.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
+      ind++;
+    }
+  }
+
   ////////////////////////////////////////////////////////////////////////////
 }
