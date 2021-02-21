e8d4a0338978d3da8a6f78e6f13ebf7fe5dca8d6
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-20 23:45:42 -0500

    Update

--- a5/per_corner_normals.cpp
+++ a5/per_corner_normals.cpp
@@ -13,18 +13,19 @@ void per_corner_normals(
   N = Eigen::MatrixXd::Zero(F.rows()*3,3);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
-  double epsilon = cos((M_PI*corner_threshold)/180);
+  const double pi = 3.14159265358979323846;
+  double epsilon = cos((pi*corner_threshold)/180);
   int num_vertices = V.rows();
   std::vector<std::vector<int> > VF;
   vertex_triangle_adjacency(F, num_vertices, VF);
   Eigen::RowVector3d nf, ng, cur_result;
   cur_result = Eigen::RowVector3d(0, 0, 0);
 
-  int i, j, k;
-  for (i=0; i<F.rows(); i++} {
+  int i, k;
+  for (i=0; i<F.rows(); i++) {
     nf = triangle_area_normal(V.row(F(i, 0)), V.row(F(i, 1)), V.row(F(i, 2)));
     for (k=0; k<F.cols(); k++) {
-      for (j : VF.row(F(i, k))) {
+      for (int j : VF[F(i, k)]) {
         ng = triangle_area_normal(V.row(F(j, 0)), V.row(F(j, 1)), V.row(F(j, 2)));
         if (nf.dot(ng) > epsilon) {
           cur_result += ng;
