a028c21f868901424d05d4f1d0a198ab6fc432d5
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-20 22:54:00 -0500

    Update

--- a5/per_vertex_normals.cpp
+++ a5/per_vertex_normals.cpp
@@ -3,36 +3,34 @@
 #include "vertex_triangle_adjacency.h"
 
 void per_vertex_normals(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  Eigen::MatrixXd & N)
+  const Eigen::MatrixXd& V,
+  const Eigen::MatrixXi& F,
+  Eigen::MatrixXd& N)
 {
-  N = Eigen::MatrixXd::Zero(V.rows(),3);
+  N = Eigen::MatrixXd::Zero(V.rows(), 3);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
   int num_vertices = V.rows();
   std::vector<std::vector<int> > VF;
-  vertex_triangle_adjacency(F, num_vertices, VF):
+  vertex_triangle_adjacency(F, num_vertices, VF);
 
-  int i, j,  ia, ib, ic;
+  int ind, ia, ib, ic;
   Eigen::RowVector3d a, b, c;
 
-  Eigen::RowVector3d
-  for (i=0; i<VF.rows(); i++) {
-    for (j : VF.row(i)) {
+  ind = 0;
+  for (std::vector<int> i : VF) {
+    for (int j : i) {
       ia = F(j, 0);
       ib = F(j, 1);
       ic = F(j, 2);
       a = V.row(ia);
       b = V.row(ib);
       c = V.row(ic);
-      N.row(i) += triangle_area_normal(a, b, c);
+      N.row(ind) += triangle_area_normal(a, b, c);
     }
-    N.row(i) = N.row(i).normalized();
+    N.row(ind) = N.row(ind).normalized();
+    ind++;
   }
-
-
-
 
   ////////////////////////////////////////////////////////////////////////////
 }
