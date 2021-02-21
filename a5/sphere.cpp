39ea679041bd328e8b7c8fac0650bcd1e491a6bd
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-20 21:37:03 -0500

    Update

--- a5/sphere.cpp
+++ a5/sphere.cpp
@@ -1,5 +1,6 @@
 #include "sphere.h"
 #include <iostream>
+// #include <math.h>
 
 void sphere(
   const int num_faces_u,
@@ -24,26 +25,23 @@ void sphere(
   NV.resize(total_vertex_num,3);
   NF.resize(total_face_num,4);
 
-  const double pi = 355/113; 
-  double x, y, z, r, u, v, theta, ft, phi, fp;
+  const double pi = 3.14159265358979323846;
+  double x, y, z, r, u, v, t, ft, p, fp;
   int i, j, ind;
   r = 1.0;
-  ft = pi/num_faces_u;
-  fp = (2*pi)/num_faces_v;
+  ft = pi/num_faces_v;
+  fp = (2*pi)/num_faces_u;
 
   ind = 0;
   for (i=0; i<(num_faces_u + 1); i++){
     for (j=0; j<(num_faces_v + 1); j++){
-      theta = ft *i;
-      phi = fp*j;
-
-      x = r*sin(theta)*cos(phi);
-      y = r*sin(theta)*sin(phi);
-      z = r*cos(theta);
+      x = -r*sin(ft *j)*cos(fp*i);
+      y = -r*sin(ft *j)*sin(fp*i);
+      z = -r*cos(ft *j);
 
       V.row(ind) = Eigen::RowVector3d(x, y, z);
-      UV(ind, 1) = i/num_faces_u;
-      UV(ind, 2) = j/num_faces_v;
+      UV(ind, 0) = ((double)i)/num_faces_u;
+      UV(ind, 1) = ((double)j)/num_faces_v;
       NV.row(ind) = Eigen::RowVector3d(x, y, z);
       ind++;
     }
@@ -58,8 +56,8 @@ void sphere(
       c4 = (i+1) * (num_faces_v+1) + j;
       c3 = c4 + 1;
       F.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
-      F.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
-      F.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
+      UF.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
+      NF.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
       ind++;
     }
   }
