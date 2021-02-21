427399406a00970f9f298a715efed68083dad08e
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-02-20 21:49:35 -0500

    Update

--- a5/sphere.cpp
+++ a5/sphere.cpp
@@ -15,52 +15,47 @@ void sphere(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here:
   // https://en.wikipedia.org/wiki/Spherical_coordinate_system
-  int total_face_num = num_faces_u * num_faces_v;
-  int total_vertex_num = (num_faces_u + 1) * (num_faces_v + 1);
 
-  V.resize(total_vertex_num,3);
-  F.resize(total_face_num,4);
-  UV.resize(total_vertex_num,2);
-  UF.resize(total_face_num,4);
-  NV.resize(total_vertex_num,3);
-  NF.resize(total_face_num,4);
+  V.resize((num_faces_u + 1) * (num_faces_v + 1),3);
+  F.resize(num_faces_u * num_faces_v,4);
+  UV.resize((num_faces_u + 1) * (num_faces_v + 1),2);
+  UF.resize(num_faces_u * num_faces_v,4);
+  NV.resize((num_faces_u + 1) * (num_faces_v + 1),3);
+  NF.resize(num_faces_u * num_faces_v,4);
 
   const double pi = 3.14159265358979323846;
   double x, y, z, r, u, v, t, ft, p, fp;
-  int i, j, ind;
+  int c1, c2, c3, c4;
+  int i, j, vind, find;
   r = 1.0;
   ft = pi/num_faces_v;
   fp = (2*pi)/num_faces_u;
 
-  ind = 0;
+  vind = 0;
+  find = 0;
   for (i=0; i<(num_faces_u + 1); i++){
     for (j=0; j<(num_faces_v + 1); j++){
       x = -r*sin(ft *j)*cos(fp*i);
       y = -r*sin(ft *j)*sin(fp*i);
       z = -r*cos(ft *j);
 
-      V.row(ind) = Eigen::RowVector3d(x, y, z);
-      UV(ind, 0) = ((double)i)/num_faces_u;
-      UV(ind, 1) = ((double)j)/num_faces_v;
-      NV.row(ind) = Eigen::RowVector3d(x, y, z);
-      ind++;
+      V.row(vind) = Eigen::RowVector3d(x, y, z);
+      UV(vind, 0) = ((double)i)/num_faces_u;
+      UV(vind, 1) = ((double)j)/num_faces_v;
+      NV.row(vind) = Eigen::RowVector3d(x, y, z);
+
+      if ((i<num_faces_u) && (j<num_faces_v)){
+        c1 = i * (num_faces_v+1) + j;
+        c2 = c1 +1;
+        c4 = (i+1) * (num_faces_v+1) + j;
+        c3 = c4 + 1;
+        F.row(find) = Eigen::RowVector4i(c1, c2, c3, c4);
+        UF.row(find) = Eigen::RowVector4i(c1, c2, c3, c4);
+        NF.row(find) = Eigen::RowVector4i(c1, c2, c3, c4);
+        find++;
+      }
+      vind++;
     }
   }
-
-  int c1, c2, c3, c4;
-  ind = 0;
-  for (i=0; i<num_faces_u; i++){
-    for (j=0; j<num_faces_v; j++){
-      c1 = i * (num_faces_v+1) + j;
-      c2 = c1 +1;
-      c4 = (i+1) * (num_faces_v+1) + j;
-      c3 = c4 + 1;
-      F.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
-      UF.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
-      NF.row(ind) = Eigen::RowVector4i(c1, c2, c3, c4);
-      ind++;
-    }
-  }
-
   ////////////////////////////////////////////////////////////////////////////
 }
