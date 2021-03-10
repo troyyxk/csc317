25cd3f283f37e27bbf09c99255a8e48ef778da0f
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-10 17:26:09 -0500

    Update

--- a6/lit.fs
+++ a6/lit.fs
@@ -18,6 +18,23 @@ void main()
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code 
-  color = vec3(1,1,1);
+  vec3 ka = vec3(0.01,0.01,0.01);
+  vec3 kd;
+  if (is_moon) {
+    kd = vec3(0.5,0.5,0.5);
+  } else {
+    kd = vec3(0,0,1);
+  }
+  vec3 ks = vec3(0.9, 0.9, 0.9);
+  vec3 n = normalize(normal_fs_in);
+  vec3 v = normalize(-view_pos_fs_in.xyz);
+  float theta = (M_PI/2.0)*animation_seconds;
+  mat4 ry = mat4(
+      cos(theta),0,-sin(theta),0,
+      0,1,0,0,
+      sin(theta),0,cos(theta),0,
+      0,0,0,1);
+  vec3 l = normalize(ry * vec4(1.0, 1.0, 1.0, 1.0)).xyz;
+  color = blinn_phong(ka, kd, ks, 1000, n, v, l);
   /////////////////////////////////////////////////////////////////////////////
 }
