74da3aa526baf6025227e419dbb674cdc7ca743d
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-17 14:51:28 -0400

    Update

--- a7/copy_skeleton_at.cpp
+++ a7/copy_skeleton_at.cpp
@@ -6,6 +6,13 @@ Skeleton copy_skeleton_at(
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
   Skeleton copy = skeleton;
+  int i;
+  int num_skeleton = skeleton.size();
+  for (i=0; i<num_skeleton; i++) {
+    copy[i].xzx[0] = A(i*3);
+    copy[i].xzx[1] = A(i*3 + 1);
+    copy[i].xzx[2] = A(i*3 + 2);
+  }
   return copy;
   /////////////////////////////////////////////////////////////////////////////
 }
