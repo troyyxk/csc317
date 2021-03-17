947c439a65918efe753dd9e4e9545291e184d5c4
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-17 14:38:50 -0400

    update

--- a7/linear_blend_skinning.cpp
+++ a7/linear_blend_skinning.cpp
@@ -9,6 +9,29 @@ void linear_blend_skinning(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  U = V;
+  int num_rest = V.rows();
+  int num_skeleton = skeleton.size();
+  U.resize(num_rest, 3);
+  int i, j;
+  Eigen::Vector4d cur;
+
+
+  for (i=0; i<num_rest; i++) {
+    cur = Eigen::Vector4d(0, 0, 0, 0);
+    for (j=0; j<num_skeleton; j++) {
+      Eigen::Vector4d vi = Eigen::Vector4d(1, 1, 1, 1);
+      if (skeleton[j].weight_index != -1) {
+        vi(0) = V(i, 0);
+        vi(1) = V(i, 1);
+        vi(2) = V(i, 2);
+        cur += W(i, skeleton[j].weight_index) * T[j] * vi;
+      }
+    }
+    U(i, 0) = base(0);
+    U(i, 1) = base(1);
+    U(i, 2) = base(2);
+  }
+  
+  return;
   /////////////////////////////////////////////////////////////////////////////
 }
