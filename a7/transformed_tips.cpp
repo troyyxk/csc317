23cc420718875a9eaebb82c3a6a34ed355919c6d
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-17 02:09:31 -0400

    update

--- a7/transformed_tips.cpp
+++ a7/transformed_tips.cpp
@@ -7,6 +7,23 @@ Eigen::VectorXd transformed_tips(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  return Eigen::VectorXd::Zero(3*b.size());
+  Eigen::VectorXd tips;
+  tips = Eigen::VectorXd::Zero(3*b.size());
+  std::vector<Eigen::Affine3d,Eigen::aligned_allocator<Eigen::Affine3d> >  T;
+  T = forward_kinematics(skeleton, T);
+
+  Bone bone;
+  skeleton[b[i]] tip;
+  int i, cur;
+  for (i=0; i<b.size(); i++) {
+    cur = b[i];
+    bone = skeleton[cur];
+    tip = T[cur] * bone.rest_T * Eigen::Vector4d(bone.length, 0, 0, 1);
+    tips[i*3 + 0] = tip[0];
+    tips[i*3 + 1] = tip[1];
+    tips[i*3 + 2] = tip[2];
+
+  }
+  return tips;
   /////////////////////////////////////////////////////////////////////////////
 }
