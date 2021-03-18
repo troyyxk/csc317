dd5130856dc185091012756dbed1c12f12bcd819
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-18 19:58:50 -0400

    Update

--- a7/forward_kinematics.cpp
+++ a7/forward_kinematics.cpp
@@ -15,12 +15,14 @@ void forward_kinematics(
     if (bone.parent_index < 0) {
       T[i] = Eigen::Affine3d::Identity();
     }
-    Eigen::Affine3d Tpi = T[bone.parent_index];
-    Eigen::Affine3d Ti = bone.rest_T;
-    Eigen::Affine3d Tii = Ti.inverse();
-    Eigen::Affine3d R = euler_angles_to_transform(bone.xzx);
+    else {
+      Eigen::Affine3d Tpi = T[bone.parent_index];
+      Eigen::Affine3d Ti = bone.rest_T;
+      Eigen::Affine3d Tii = Ti.inverse();
+      Eigen::Affine3d R = euler_angles_to_transform(bone.xzx);
 
-    T[i] = Tpi * Ti * R * Tii;
+      T[i] = Tpi * Ti * R * Tii;
+    }
 
 
   }
