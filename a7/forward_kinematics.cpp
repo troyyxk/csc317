5eef7beef174ec76d3e48898c02a9b072a63af72
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-18 19:39:16 -0400

    Update

--- a7/forward_kinematics.cpp
+++ a7/forward_kinematics.cpp
@@ -3,20 +3,20 @@
 #include <functional> // std::function
 
 void forward_kinematics(
-  const Skeleton & skeleton,
-  std::vector<Eigen::Affine3d,Eigen::aligned_allocator<Eigen::Affine3d> > & T)
+  const Skeleton& skeleton,
+  std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d> >& T)
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  T.resize(skeleton.size(),Eigen::Affine3d::Identity());
+  T.resize(skeleton.size(), Eigen::Affine3d::Identity());
   int i;
-  for (i=0; i<skeleton.size(); i++){
+  for (i = 0; i < skeleton.size(); i++) {
     Bone bone = skeleton[i];
-    if (bone.parent_index < 0){
+    if (bone.parent_index < 0) {
       T[i] = Eigen::Affine3d::Identity();
     }
     Eigen::Affine3d Tpi = T[bone.parent_index];
-    Eigen::Affine3d Ti = rest_T;
+    Eigen::Affine3d Ti = bone.rest_T;
     Eigen::Affine3d Tii = Ti.inverse();
     Eigen::Affine3d R = euler_angles_to_transform(bone.xzx);
 
