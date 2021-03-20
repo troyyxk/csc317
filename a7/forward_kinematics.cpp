2bea9edb95255bd6666fc00ef77ba5b9de1c0f93
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-19 20:50:49 -0400

    Updaet

--- a7/forward_kinematics.cpp
+++ a7/forward_kinematics.cpp
@@ -2,6 +2,30 @@
 #include "euler_angles_to_transform.h"
 #include <functional> // std::function
 
+Eigen::Affine3d recursion(
+  const Skeleton& skeleton,
+  std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d> >& T,
+  int cur)
+{
+  Bone bone = skeleton[cur];
+  Eigen::Affine3d Tpi, Ti, Tii, R;
+  if (bone.parent_index < 0){
+    Tpi = Eigen::Affine3d::Identity();
+    return Tpi;
+  }
+  else if (skeleton[bone.parent_index].parent_index < 0){
+    Tpi = T[bone.parent_index];
+  }
+  else {
+    Tpi = recursion(skeleton, T, bone.parent_index);
+  }
+  Ti = bone.rest_T;
+  Tii = Ti.inverse();
+  R = euler_angles_to_transform(bone.xzx);
+
+  return Tpi * Ti * R * Tii;
+}
+
 void forward_kinematics(
   const Skeleton& skeleton,
   std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d> >& T)
@@ -9,6 +33,7 @@ void forward_kinematics(
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
   T.resize(skeleton.size(), Eigen::Affine3d::Identity());
+  Eigen::Affine3d Tpi, Ti, Tii, R;
   int i;
   for (i = 0; i < skeleton.size(); i++) {
     Bone bone = skeleton[i];
@@ -16,12 +41,7 @@ void forward_kinematics(
       T[i] = Eigen::Affine3d::Identity();
     }
     else {
-      Eigen::Affine3d Tpi = T[bone.parent_index];
-      Eigen::Affine3d Ti = bone.rest_T;
-      Eigen::Affine3d Tii = Ti.inverse();
-      Eigen::Affine3d R = euler_angles_to_transform(bone.xzx);
-
-      T[i] = Tpi * Ti * R * Tii;
+      T[i] = recursion(skeleton, T, i);
     }
 
 
