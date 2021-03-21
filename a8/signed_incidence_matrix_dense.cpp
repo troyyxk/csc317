386d640bc1bec0edfd2045fb4d9add818672a449
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-21 03:12:43 -0400

    Update

--- a8/signed_incidence_matrix_dense.cpp
+++ a8/signed_incidence_matrix_dense.cpp
@@ -8,5 +8,19 @@ void signed_incidence_matrix_dense(
   //////////////////////////////////////////////////////////////////////////////
   // Replace with your code
   A = Eigen::MatrixXd::Zero(E.rows(),n);
+  int i, j, e, k;
+  int edge_amount = E.rows();
+  for (e=0; e<edge_amount; e++){
+    i = E(e, 0);
+    j = E(e, 1);
+    for (k=0; k<n; k++){
+      if (k == i) {
+        A(e, k) = 1.0;
+      }
+      if (k == e) {
+        A(e, k) = -1.0;
+      }
+    }
+  }
   //////////////////////////////////////////////////////////////////////////////
 }
