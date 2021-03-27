46e28ed3d59211343fc28a661aa08de9140c4c4e
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-26 21:15:19 -0400

    Update

--- a8/fast_mass_springs_step_sparse.cpp
+++ a8/fast_mass_springs_step_sparse.cpp
@@ -19,9 +19,18 @@ void fast_mass_springs_step_sparse(
 {
   //////////////////////////////////////////////////////////////////////////////
   // Replace with your code
+  int i;
+  int e_size = E.rows();
+  double w=1e10;
+  const Eigen::MatrixXd y = (1/pow(delta_t, 2))*M  * (2*Ucur - Uprev) + fext;
+  Eigen::MatrixXd d;
   for(int iter = 0;iter < 50;iter++)
   {
-    const Eigen::MatrixXd l = Ucur;
+    d = Eigen::MatrixXd::Zero(e_size, 3);
+    for (i=0; i<e_size; i++) {
+      d.row(i) = r(i)*(Unext.row(E(i, 0)) - Unext.row(E(i, 1))).normalized();
+    }
+    const Eigen::MatrixXd l = k*A.transpose()*d + y +  + w*C.transpose()*C*V;
     Unext = prefactorization.solve(l);
   }
   //////////////////////////////////////////////////////////////////////////////
