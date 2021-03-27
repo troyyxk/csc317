de0dd556344c57c13819ae3fa80a8911808f4ec0
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-03-26 21:07:18 -0400

    update

--- a8/fast_mass_springs_precomputation_sparse.cpp
+++ a8/fast_mass_springs_precomputation_sparse.cpp
@@ -17,11 +17,41 @@ bool fast_mass_springs_precomputation_sparse(
 {
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code
-  std::vector<Eigen::Triplet<double> > ijv;
+  // std::vector<Eigen::Triplet<double> > ijv;
   const int n = V.rows();
-  for(int i = 0;i<n;i++) ijv.emplace_back(i,i,1);
+  // for(int i = 0;i<n;i++) ijv.emplace_back(i,i,1);
+
+  int e_size = E.rows();
+  int v_size = V.rows();
+  int i, j;
+  r.resize(e_size);
+  for (i=0; i<e_size; i++) {
+    r(i) = (V.row(E(i, 0)) - V.row(E(i, 1))).norm();
+  }
+
+  std::vector<Eigen::Triplet<double> > ijv;
+  M.resize(v_size, v_size);
+  for(i = 0;i<v_size;i++) ijv.emplace_back(i,i,m(i));
+  M.setFromTriplets(ijv.begin(),ijv.end());
+
+  A.resize(e_size, v_size);
+  signed_incidence_matrix_sparse(v_size, E, A);
+
+  int b_size = b.rows();
+  std::vector<Eigen::Triplet<double> > ijv0;
+  C.resize(b_size, v_size);
+  for (i=0; i<b_size; i++) {
+    for (j=0; j<v_size; j++) {
+      if(j == b(i)) {
+        ijv0.emplace_back(i,j,1);
+      }
+    }
+  }
+  C.setFromTriplets(ijv0.begin(),ijv0.end());
+
   Eigen::SparseMatrix<double> Q(n,n);
-  Q.setFromTriplets(ijv.begin(),ijv.end());
+  double w=1e10;
+  Q = k*(A.transpose())*A + (1/pow(delta_t, 2))*M + w*C.transpose()*C;
   /////////////////////////////////////////////////////////////////////////////
   prefactorization.compute(Q);
   return prefactorization.info() != Eigen::NumericalIssue;
--- a8/signed_incidence_matrix_sparse.cpp
+++ a8/signed_incidence_matrix_sparse.cpp
@@ -9,7 +9,24 @@ void signed_incidence_matrix_sparse(
   //////////////////////////////////////////////////////////////////////////////
   // Replace with your code
   std::vector<Eigen::Triplet<double> > ijv;
-  ijv.emplace_back(0,0,1234.5678);
+  // ijv.emplace_back(0,0,1234.5678);
+  // A.resize(E.rows(),n);
+  // A.setFromTriplets(ijv.begin(),ijv.end());
+
+  int e_size = E.rows();
+  int i, j, e, k;
+  for (e=0; e<e_size; e++){
+    i = E(e, 0);
+    j = E(e, 1);
+    for (k=0; k<n; k++){
+      if (k == i) {
+        ijv.emplace_back(e, k, 1.0);
+      }
+      if (k == j) {
+        ijv.emplace_back(e, k, -1.0);
+      }
+    }
+  }
   A.resize(E.rows(),n);
   A.setFromTriplets(ijv.begin(),ijv.end());
   //////////////////////////////////////////////////////////////////////////////
